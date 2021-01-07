44b56f0fe0be210294d7085f52d6b20d0a7fa10d
Vincent C. Mader <vincent.mader@icloud.com>
2021-01-08 00:32:44 +0100

    minor

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import double_pendulum
 import n_body
 import gas_in_a_box
 
-# double_pendulum.main(run_integrator=False)
+double_pendulum.main(run_integrator=False)
 
-gas_in_a_box.main(run_integrator=False)
+# gas_in_a_box.main(run_integrator=False)
 
 # n_body.main(run_integrator=False)
