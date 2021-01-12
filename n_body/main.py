ebce75c0998f1ab52859054f5a956fafb0267bef
Vincent C. Mader <vincent.mader@icloud.com>
2021-01-12 01:48:14 +0100

    continued on n body solver

--- n_body/main.py
+++ n_body/main.py
@@ -1,20 +1,43 @@
 import numpy as np
+from numpy import sqrt
 
 from .integrate import main as integrate
 from .display import main as display
 
 
+M, G = 1, 1
+
+
+def kepler_velocity(r):
+    return np.sqrt(G * M / r)
+
+
 y0 = [
-    0, 0, 0, 0, 0, 0,  # 3 position & 3 velocity values for 1. body
-    0, 1, 0, 1, 0, 0,  # 3 position & 3 velocity values for 2. body
+    M,    1,   0, 0, 0,   0, 0, 0,  # mass, radius, 3 pos. & 3 vel. values
+    # 1e-6, 1,   0.25, 0, 0,   0, kepler_velocity(0.25), 0,
+    # 1e-6, 1,   1, 0, 0,   0, 1, 0,
+    # 1e-6, 1,   1.25, 0, 0,   0, kepler_velocity(1.25), 0,
+    # 1e-6, 1,   1.5, 0, 0,   0, kepler_velocity(1.5), 0,
+    # 1e-6, 1,   1.75, 0, 0,   0, kepler_velocity(1.75), 0,
+    # 1e-6, 1,   2, 0, 0,   0, kepler_velocity(2), 0,
+    # 1e-6, 1,   2, 1, 0,   0, kepler_velocity(4), 0,
+
+    1e-5, 1,   sqrt(2), 0, 0,   0, kepler_velocity(10), 0,
+    1e-5, 1,   -sqrt(2), 0, 0,   0, -kepler_velocity(10), 0,
+    1e-5, 1,   0, sqrt(2), 0,   -kepler_velocity(10), 0, 0,
+    1e-5, 1,   0, -sqrt(2), 0,   kepler_velocity(10), 0, 0,
+
+    # 1e-5, 1,   1, 1, 0,   0,  kepler_velocity(10), 0,
+    # 1e-5, 1,   -1, 1, 0,   0,  -kepler_velocity(10), 0,
+    # 1e-5, 1,   1, -1, 0,    kepler_velocity(10), 0, 0,
+    # 1e-5, 1,   -1, -1, 0,     kepler_velocity(10), 0, 0,
 ]
-ms = [1000, 1]
 
 
-def main(steps=500, run_integrator=True):
+def main(nr_of_steps=1000, run_integrator=True):
 
     if run_integrator:
-        ys = integrate(y0, ms, steps)
+        ys = integrate(y0, nr_of_steps)
         np.savetxt('./n_body/out/ys.txt', ys)
     else:
         ys = np.loadtxt('./n_body/out/ys.txt')
