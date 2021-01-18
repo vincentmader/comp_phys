3890f3ef756e1c86f2844570f905f19bb5f5dea0
Vincent C. Mader <vincent.mader@icloud.com>
2021-01-18 18:56:17 +0100

    set double pendulum as default

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import double_pendulum
 import n_body
 import gas_in_a_box
 
-# double_pendulum.main(run_integrator=False)
+double_pendulum.main(run_integrator=False)
 
 # gas_in_a_box.main(run_integrator=True)
 # gas_in_a_box.live()
