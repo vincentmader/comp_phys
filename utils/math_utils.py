1d25f3e236db4d12cd5f3d09ce243887340a7d4f
Vincent C. Mader <vincent.mader@icloud.com>
2021-02-07 22:15:13 +0100

    added coordinate transform functions

--- utils/math_utils.py
+++ utils/math_utils.py
@@ -3,3 +3,22 @@ import numpy as np
 
 def l2_norm(vec):
     return np.sqrt(sum([i**2 for i in vec]))
+
+
+def transform_coords_cart2pol(x, y):
+    r = np.sqrt(x**2 + y**2)
+    phi = np.arctan2(y, x)
+    return r, phi
+
+
+def transform_coords_pol2cart(r, phi):
+    x = r * np.cos(phi)
+    y = r * np.sin(phi)
+    return x, y
+
+
+def rotate_2D_cartesian_vector(x, y, dphi):
+    r, phi = transform_coords_cart2pol()
+    phi += dphi
+    x, y = transform_coords_pol2cart(r, phi)
+    return x, y
